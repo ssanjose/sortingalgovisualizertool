530f3dbd474f7da8b8b9f497d052f7327ea42250
ssanjose <ssanjose@academic.rrc.ca>
2022-02-01 17:52:16 -0600

    Added animation handling for bubble sort animation

--- src/SortingAlgoVisualizerTool/Animations/animationHandler.js
+++ src/SortingAlgoVisualizerTool/Animations/animationHandler.js
@@ -1,11 +1,11 @@
-import { mergeSortAnimation } from "../Algorithms/sortingAlgorithm";
+import { bubbleSortAnimation, mergeSortAnimation } from "../Algorithms/sortingAlgorithm";
 
 //  Animation function to handle animations in the sorting algorithm visualization tool.
 export const animationHandler = (array, algorithm, config) => {
   let animations = [];
   switch (algorithm) {
     case 'bubbleSort':
-      // bubbleSort(array, animations);
+      bubbleSort(array, config);
       break;
     case 'insertionSort':
       // insertionSort(array, animations);
@@ -22,27 +22,68 @@ export const animationHandler = (array, algorithm, config) => {
   return animations;
 }
 
+//  Merge sort function to call the merge sort animation function and handle animation.
 const mergeSort = (array, config) => {
   const animations = mergeSortAnimation(array);
+  const arrayBars = document.getElementsByClassName('array-bar');
 
   for (let i = 0; i < animations.length; i++) {
-    const arrayBars = document.getElementsByClassName('array-bar');
-    const isColorChange = i % 3 !== 2;
+    let isColorChange = i % 3 !== 2;
     if (isColorChange) {
-      const [barOneIdx, barTwoIdx] = animations[i];
-      const barOneStyle = arrayBars[barOneIdx].style;
-      const barTwoStyle = arrayBars[barTwoIdx].style;
-      const color = i % 3 === 0 ? config.secondaryColor : config.primaryColor;
-      setTimeout(() => {
-        barOneStyle.backgroundColor = color;
-        barTwoStyle.backgroundColor = color;
-      }, i * config.animationSpeed);
+      let [barOneIdx, barTwoIdx] = animations[i];
+      let barOneStyle = arrayBars[barOneIdx].style;
+      let barTwoStyle = arrayBars[barTwoIdx].style;
+      let color = i % 3 === 0 ? config.secondaryColor : config.primaryColor;
+
+      setColorChange(barOneStyle, barTwoStyle, color, i, config);
     } else {
-      setTimeout(() => {
-        const [barOneIdx, newHeight] = animations[i];
-        const barOneStyle = arrayBars[barOneIdx].style;
-        barOneStyle.height = `${newHeight}px`;
-      }, i * config.animationSpeed);
+      let [barOneIdx, newHeight] = animations[i];
+      let barOneStyle = arrayBars[barOneIdx].style;
+
+      setHeightChange(barOneStyle, newHeight, i, config);
     }
   }
+}
+
+//  Bubble sort function to call the bubble sort animation function and handle animation.
+const bubbleSort = (array, config) => {
+  const animations = bubbleSortAnimation(array);
+  const arrayBars = document.getElementsByClassName('array-bar');
+
+  for (let i = 0; i < animations.length; i++) {
+    if (i % 4 === 0 || i % 4 === 1) {
+      let [barOneIdx, barTwoIdx] = animations[i];
+      let barOneStyle = arrayBars[barOneIdx].style;
+      let barTwoStyle = arrayBars[barTwoIdx].style;
+      let color = i % 2 !== 1 ? config.secondaryColor : config.primaryColor;
+
+      setColorChange(barOneStyle, barTwoStyle, color, i, config);
+    } else if (i % 4 === 2) {
+      let [barOneIdx, newHeight] = animations[i];
+      let barOneStyle = arrayBars[barOneIdx].style;
+
+      setHeightChange(barOneStyle, newHeight, i, config);
+    } else if (i % 4 === 3) {
+      let [barTwoIdx, newHeight] = animations[i];
+      let barTwoStyle = arrayBars[barTwoIdx].style;
+
+      setHeightChange(barTwoStyle, newHeight, i, config);
+    }
+  }
+
+}
+
+// Takes the both bar styles, color, index and config and sets a timeout to change the bar style.
+const setColorChange = (barOneStyle, barTwoStyle, color, index, config) => {
+  setTimeout(() => {
+    barOneStyle.backgroundColor = color;
+    barTwoStyle.backgroundColor = color;
+  }, index * config.animationSpeed);
+}
+
+// Takes a bar style, height, index and config and sets a timeout to change the bar height.
+const setHeightChange = (barStyle, height, index, config) => {
+  setTimeout(() => {
+    barStyle.height = `${height}px`;
+  }, index * config.animationSpeed);
 }
