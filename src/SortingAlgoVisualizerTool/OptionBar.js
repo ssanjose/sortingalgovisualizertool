782e048c752f9e6f74efccef7795946f02d665b0
ssanjose <ssanjose@academic.rrc.ca>
2022-01-31 21:36:00 -0600

    Copies Clement's merge sorting algorithm and use in this project

--- src/SortingAlgoVisualizerTool/OptionBar.js
+++ src/SortingAlgoVisualizerTool/OptionBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export const OptionBar = (props) => {
-    return (
-        <>
-            <nav>
-                <ul id="options">
-                    <li>Check</li>
-                </ul>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav id='optionBar'>
+        <ul id="options">
+          <li>Check</li>
+        </ul>
+      </nav>
+    </>
+  )
 }
