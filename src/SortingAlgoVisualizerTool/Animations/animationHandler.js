430bbff519923195b3e739a628fb9c5f556b7785
ssanjose <ssanjose@academic.rrc.ca>
2022-02-04 15:45:49 -0600

    Adjusted selection sort to use new helper methods

--- src/SortingAlgoVisualizerTool/Animations/animationHandler.js
+++ src/SortingAlgoVisualizerTool/Animations/animationHandler.js
@@ -1,5 +1,5 @@
 import { bubbleSortAnimation, insertionSortAnimation, mergeSortAnimation, selectionSortAnimation } from "../Algorithms/sortingAlgorithm";
-import { setArrayColorChange, setColorChange, setHeightChange, setSingleColorChange } from "../helperMethods";
+import { setArrayColorChange, setColorChange, setColorChangeV2, setHeightChange, setHeightChangeV2, setSingleColorChange } from "../helperMethods";
 
 //  Animation function to handle animations in the sorting algorithm visualization tool.
 export const animationHandler = (array, algorithm, config) => {
@@ -106,44 +106,20 @@ const selectionSort = (array, config) => {
   const arrayBars = document.getElementsByClassName('array-bar');
 
   for (let i = 0; i < animations.length; i++) {
-    // if (i % 4 === 0 || i % 4 === 3) {
-    //   let [barOneIdx, barTwoIdx] = animations[i];
-    //   let barOneStyle = arrayBars[barOneIdx].style;
-    //   let barTwoStyle = arrayBars[barTwoIdx].style;
-    //   let color = i % 2 === 0 ? config.secondaryColor : config.primaryColor;
-
-    //   setColorChange(barOneStyle, barTwoStyle, color, i, config);
-    // } else if (i % 4 === 1) {
-    //   let [barOneIdx, newHeight] = animations[i];
-    //   let barOneStyle = arrayBars[barOneIdx].style;
-
-    //   setHeightChange(barOneStyle, newHeight, i, config);
-    // } else if (i % 4 === 2) {
-    //   let [barTwoIdx, newHeight] = animations[i];
-    //   let barTwoStyle = arrayBars[barTwoIdx].style;
-
-    //   setHeightChange(barTwoStyle, newHeight, i, config);
-    // }
     if (i % 3 === 0) {
       let [barBoundIdx, selectionArrayIdxs] = animations[i];
 
-      let barBoundStyle = arrayBars[barBoundIdx].style;
-      setSingleColorChange(barBoundStyle, config.boundColor, i, config.animationSpeed);
-      setArrayColorChange(arrayBars, selectionArrayIdxs, config.secondaryColor, i, config.animationSpeed);
+      setColorChangeV2(arrayBars, [[barBoundIdx, config.boundColor]], config.animationSpeed);
+      setColorChangeV2(arrayBars, selectionArrayIdxs.map(idx => [idx, config.secondaryColor]), config.animationSpeed);
     } else if (i % 3 === 1) {
       let [barBoundIdx, selectionArrayIdxs] = animations[i];
 
-      let barBoundStyle = arrayBars[barBoundIdx].style;
-      setSingleColorChange(barBoundStyle, config.primaryColor, i, config.animationSpeed);
-      setArrayColorChange(arrayBars, selectionArrayIdxs, config.primaryColor, i, config.animationSpeed);
+      setColorChangeV2(arrayBars, [[barBoundIdx, config.primaryColor]], config.animationSpeed);
+      setColorChangeV2(arrayBars, selectionArrayIdxs.map(idx => [idx, config.primaryColor]), config.animationSpeed);
     } else {
       if (animations[i][0] == "swap") {
         let [_, barOne, barTwo] = animations[i];
-        let barOneStyle = arrayBars[barOne[0]].style;
-        let barTwoStyle = arrayBars[barTwo[0]].style;
-
-        setHeightChange(barOneStyle, barOne[1], i, config);
-        setHeightChange(barTwoStyle, barTwo[1], i, config);
+        setHeightChangeV2(arrayBars, [barOne, barTwo], config.animationSpeed);
       }
     }
   }
